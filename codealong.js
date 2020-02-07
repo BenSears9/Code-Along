51da841a634476d8aee16638a608c15ceb893ce3
BenSears9 <Beb.brewster.sears@gmail.com>
2020-02-07 16:40:50 -0500

    Octo fri homework

--- codealong.js
+++ codealong.js
@@ -1,18 +1,21 @@
-function printPrimes(){
-    for(var i = 0; i < 1000; i++){
-        if(i <=3){
-            console.log(i);
-        } else if(i % 3 != 0 || i % 3 != 0){
-            //the number is more than 3 
-            var j = 5;
-            while(j*j <= i){
-                if(i%j === 0 || i%( j + 2) === 0){
-                    return false:
-                }
-                j += 6;
-                console.log(i);
-            }
-        }
-    }
-}
-printPrimes();
+// function printPrimes(){
+//     for(var i = 0; i < 1000; i++){
+//         if(i <=3){
+//             console.log(i);
+//         } else if(i % 3 != 0 || i % 3 != 0){
+//             //the number is more than 3 
+//             var j = 5;
+//             while(j*j <= i){
+//                 if(i%j === 0 || i%( j + 2) === 0){
+//                     return false:
+//                 }
+//                 j += 6;
+//                 console.log(i);
+//             }
+//         }
+//     }
+// }
+// printPrimes();
+
+// document.getElementById("title").style.color ="black";
+
