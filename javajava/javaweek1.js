3ab7a3be1e215e252f8a3fd082e0c91386406a57
BenSears9 <Beb.brewster.sears@gmail.com>
2020-02-11 15:56:31 -0500

    feb eleven

---
+++ javajava/javaweek1.js
@@ -0,0 +1,27 @@
+// var x = "trout";
+// var y = 6
+// var z = (x *= y);
+// console.log(z);
+
+// var x = 2
+// var y = (x + 5);
+// var z = (y * 2);
+// console.log(z);
+// var a = (z % 3);
+// console.log(a);
+// if(a = 2, a < 10, a++); {
+//     else()
+//     console.log(a);
+// }
+// var i = 1
+// while(i <= 50){
+//     if(i % 2===0){
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+// var jj = ["jason", "glip", "random", 5, 7];
+// jj.toString();
+// console.log(jj);
+
---
+++ quiz1.js
@@ -0,0 +1,53 @@
+// Problem 1:
+// 	You are going to make a very simple calculator. 
+// 	You will create a page with inputs for the user.
+// 	You will take two numbers from user input and divide them. 
+// 	You will then display that result, with two decimal points. 
+// 	Hint: make sure that your user can’t break your calculator
+
+// Problem 2:
+// 	You will be proofreading some texts.
+// 	Given any string, find the longest word and log the length of that word.
+// 	Next, without changing the original array remove any words that are less than four characters, log the removed words, and the text after these changes.
+// 	Bonus: can you show all of this to the user? 
+// var strSplit = "The text I will be proofreading" .split(' ');
+// var arr = ["Im running out of time"];
+// for (var i = 0; i < array.length; i++){
+//     console.log(arr);
+// }
+
+// function findTheLongestWord(str) {
+//     var strSplit = str.split(' ');
+//     var longestWord = 0;
+//     for(var i = 0; i < strSplit.length; i++){
+//         if(strSplit[i].length > longestWord){
+//             longestWord = strSplit[i].length;
+//         }
+//     }
+//     return longestWord;
+// }
+// findTheLongestWord("Insert the text to be proofread here");
+
+// Problem 3:
+// 	You will make a barn object
+// 	This object should have three properties(these can be whatever you want i.e. what color is the barn?)
+// 	This object should have two methods(what can you do in a barn?)
+// 	Use one method to change a property of the object(for example, have a paint barn method and make the barn blue, not red)
+
+// var barn = {color:"red", roof:"tin roof", walls:4}
+//     paintTheRoof: function(paintTheRoof) {
+//         return this.color + " " + this.roof;
+//     }
+//     repaintTheRoof: function(repaintTheRoof) {
+//         var x = color;
+//         x = orange;
+//         return this.color + " " + this.roof;
+//     }
+
+// Bonus:
+// 	Using regex, let a user make a new password with these requirements:
+// More than 6 characters long
+// Uses at least one number
+// Uses at least one capital letter
+// Password IS NOT “password”
+
